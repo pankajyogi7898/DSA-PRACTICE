c8825cba18c95e4926d684814dc14b26d44a9fee
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-18 12:49:16 +0530

    even odd

--- Conditional statements/index.js
+++ Conditional statements/index.js
@@ -1,14 +1,24 @@
 // 1. accept two number and write a program to find and return greatest of the two numbers
 
 const propmt = require("prompt-sync")()
-let n1 = Number(propmt("Enter first number: "));
-let n2 = Number(propmt("Enter second number: "));
+// let n1 = Number(propmt("Enter first number: "));
+// let n2 = Number(propmt("Enter second number: "));
 
-if (n1 > n2) {
-    console.log(n1)
-}
-else {
-    console.log(n2)
-}
+// if (n1 > n2) {
+//     console.log(n1)
+// }
+// else {
+//     console.log(n2)
+// }
 
+// 2  Write a program that takes an integer as input and checks whether the number is even or odd. • If the number is even, return "Even". • If the number is odd, return "Odd".
+
+// let num = Number(propmt("Enter number: "));
+
+// if (num % 2 == 0) {
+//     console.log("Even")
+// }
+// else {
+//     console.log("Odd")
+// }
 
