36dad8cefbf16430a3718b17df42f3f5c91b3052
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-19 17:02:52 +0530

    sum of n natural numbers

--- Loops/index.js
+++ Loops/index.js
@@ -8,8 +8,19 @@ const propmt = require("prompt-sync")()
 
 
 // 2. Given an integer n, print all natural numbers starting from n down to 1.
+// let number = Number(propmt("Enter Number: "));
+// for (let i = number; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// 3. Write a program that accepts an integer "n" and calculates the sum of the first "n" natural numbers. Natural numbers start from 1 and go up to "n".
 let number = Number(propmt("Enter Number: "));
-for (let i = number; i >= 1; i--) {
-    console.log(i)
+let sum = 0;
+for (let i = 1; i <= number; i++) {
+    sum = sum + i;
 }
+console.log(sum);
+
+
+
 
