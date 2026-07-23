a7139bb9c2609e9ac85b9b40161e431299cf1d99
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-23 17:30:42 +0530

    strong num or not

--- Loops/Loops_Problem.js
+++ Loops/Loops_Problem.js
@@ -25,12 +25,33 @@ const prompt = require("prompt-sync")()
 
 //3. Write a program that accepts two integers, a and b, as input and calculates the value of a raised to the power of b (a^b). For example, if a = 2 and b = 3, the result is 8 (since 2^3 = 8).
 
-const num1 = Number(prompt("Enter first Number:"));
-const num2 = Number(prompt("Enter second Number:"));
-let result = 1
-for (let i = 1; i <= num2; i++) {
-    result = result * num1
+// const num1 = Number(prompt("Enter first Number:"));
+// const num2 = Number(prompt("Enter second Number:"));
+// let result = 1
+// for (let i = 1; i <= num2; i++) {
+//     result = result * num1
+// }
+// console.log(result)
+
+//4. A number is called a strong number if the sum of the factorial of its digits is equal to the number itself. Write a program that accepts a number and checks if it is a strong number. • If n is a strong number, print "Yes". • Otherwise, print "No".
+
+let n = Number(prompt("Enter Number:"));
+let original_num = n;
+let sum = 0;
+
+while (n > 0) {
+    let digit = n % 10
+
+    let fact = 1;
+    for (let i = 1; i <= digit; i++) {
+        fact = fact * i
+    }
+    sum = fact + sum
+    n = Math.floor(n / 10)
 }
-console.log(result)
-
-
+if (sum === original_num) {
+    console.log("Yes,it is strong num")
+}
+else {
+    console.log(`Not Strong number ${n}`)
+}
