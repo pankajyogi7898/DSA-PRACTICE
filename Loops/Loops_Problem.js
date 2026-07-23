9f38a995a90bc5564363ee149d8b996803ca5abe
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-23 17:51:50 +0530

    break statement

--- Loops/Loops_Problem.js
+++ Loops/Loops_Problem.js
@@ -35,23 +35,32 @@ const prompt = require("prompt-sync")()
 
 //4. A number is called a strong number if the sum of the factorial of its digits is equal to the number itself. Write a program that accepts a number and checks if it is a strong number. • If n is a strong number, print "Yes". • Otherwise, print "No".
 
-let n = Number(prompt("Enter Number:"));
-let original_num = n;
-let sum = 0;
+// let n = Number(prompt("Enter Number:"));
+// let original_num = n;
+// let sum = 0;
 
-while (n > 0) {
-    let digit = n % 10
+// while (n > 0) {
+//     let digit = n % 10
 
-    let fact = 1;
-    for (let i = 1; i <= digit; i++) {
-        fact = fact * i
-    }
-    sum = fact + sum
-    n = Math.floor(n / 10)
-}
-if (sum === original_num) {
-    console.log("Yes,it is strong num")
-}
-else {
-    console.log(`Not Strong number ${n}`)
+//     let fact = 1;
+//     for (let i = 1; i <= digit; i++) {
+//         fact = fact * i
+//     }
+//     sum = fact + sum
+//     n = Math.floor(n / 10)
+// }
+// if (sum === original_num) {
+//     console.log("Yes,it is strong num")
+// }
+// else {
+//     console.log(`Not Strong number ${n}`)
+// }
+
+//5. use a break statement - jab loops chalta rhta hai aur hame koi condition par loop ko rokna hota hai to vaha par break use krte hai
+
+//for example - koi class mein 10 student hai usko "ram" name ke ldke ko dhoodhna hai attedance le rhe hai teacher vo student 6th position pe mil gya...uske baad uske aage hi nhi jayega to aaise condition mein break ka use krte hai
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) break;
+    console.log(i)
 }
