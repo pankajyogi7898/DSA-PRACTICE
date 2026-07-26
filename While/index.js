57a9885cb9c01124b9a126eef327c54ceb1adcf6
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-26 18:17:59 +0530

    reverse number

--- While/index.js
+++ While/index.js
@@ -1,13 +1,24 @@
+let prompt = require("prompt-sync")()
 //1 Write a program that takes an integer "n" as input and computes the sum of its digits. • The input number is always positive.
 
+// let n = Number(prompt("Enter Number : "))
 
-let prompt = require("prompt-sync")()
+// let sum = 0;
+// while (n > 0) {
+//     let rem = n % 10
+//     sum = sum + rem
+//     n = Math.floor(n / 10)
+// }
+// console.log(sum)
+
+//2. Write a program that accepts a positive integer "n" as input and return the reverse of the number.
+
+
 let n = Number(prompt("Enter Number : "))
-
-let sum = 0;
+let reverse = 0
 while (n > 0) {
     let rem = n % 10
-    sum = sum + rem
+    reverse = rem + (reverse * 10)
     n = Math.floor(n / 10)
 }
-console.log(sum)
+console.log(reverse)
