023fc0c0a1ba2ec991bfe42f6dd0306afe609506
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-26 19:19:10 +0530

    Automorphic number checking

--- While/index.js
+++ While/index.js
@@ -27,24 +27,41 @@ let prompt = require("prompt-sync")()
 //invalid isbn no  - 0201314524 , 0132350883 , 0201633613
 
 
+
+// let num = Number(prompt("Enter Number : "))
+// let print = num
+// let copy = num;
+// let count = 0;
+// while (num > 0) {
+//     count++;
+//     num = Math.floor(num / 10)
+// }
+// if (count != 10) console.log("Invalid ISBN number");
+// else {
+//     let ans = 0;
+//     while (copy > 0) {
+//         let digit = copy % 10
+//         ans = ans + (count * digit)
+//         count--
+//         copy = Math.floor(copy / 10)
+//     }
+//     console.log(ans % 11 === 0 ? `${print} is Valid ISBN` : `${print} is Invalid ISBN`)
+// }
+
+
+//4. Write a program that accepts a number n and checks whether it is an Automorphic Number or not.
+// An Automorphic Number is a number whose square ends with the same digits as the number itself.
+
+
 let num = Number(prompt("Enter Number : "))
-let print = num
 let copy = num;
 let count = 0;
+let sq = num * num
 while (num > 0) {
     count++;
     num = Math.floor(num / 10)
 }
-if (count != 10) console.log("Invalid ISBN number");
+if (sq % Math.pow(10, count) == copy) console.log("AUTOMORPHIC Number");
 else {
-    let ans = 0;
-    while (copy > 0) {
-        let digit = copy % 10
-        ans = ans + (count * digit)
-        count--
-        copy = Math.floor(copy / 10)
-    }
-    console.log(ans % 11 === 0 ? `${print} is Valid ISBN` : `${print} is Invalid ISBN`)
+    console.log("Not AUTOMORPHIC Number");
 }
-
-
