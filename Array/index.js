b2ac7fe86178421e0763a1f3f2b96c8c7d7e27c1
PANKAJ NATH <pyogi8461@gmail.com>
2026-08-01 21:44:54 +0530

    Sum of Array elements

--- Array/index.js
+++ Array/index.js
@@ -1,12 +1,27 @@
-//1. Accept value from the user  and assign in the array
-
 const prompt = require("prompt-sync")()
 
-let n = Number(prompt("Enter a Number : "));
+//1. Accept value from the user  and assign in the array
 
+
+// let n = Number(prompt("Enter a Number : "));
+
+// let arr = new Array(n)
+
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = Number(prompt("Enter a Value : "))
+// }
+// console.log(arr);
+
+//2. Sum of Array Elements
+
+let n = Number(prompt("Enter a Number : "))
 let arr = new Array(n)
-
+let sum = 0
 for (let i = 0; i < arr.length; i++) {
     arr[i] = Number(prompt("Enter a Value : "))
+    sum = sum + arr[i]
 }
-console.log(arr);
+console.log(arr)
+console.log(`Sum of Array Elements : ${sum}`)
+
+
